16c48f75560b050177957c64be5ac832bee8d97f
RaycksonCosta <raycksonaraujo@gmail.com>
2022-03-15 11:45:23 -0300

    Refatorando arquitetura do projeto

--- app/src/main/java/com/br/natanfc/filmesflix/framework/viewmodel/MovieListViewModel.kt
+++ app/src/main/java/com/br/natanfc/filmesflix/framework/viewmodel/MovieListViewModel.kt
@@ -1,10 +1,10 @@
-package com.br.natanfc.filmesflix.viewmodel
+package com.br.natanfc.filmesflix.framework.viewmodel
 
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.br.natanfc.filmesflix.api.MovieRestApiTask
+import com.br.natanfc.filmesflix.framework.api.MovieRestApiTask
 import com.br.natanfc.filmesflix.data.MovieRepository
 import com.br.natanfc.filmesflix.domain.Movie
 import com.br.natanfc.filmesflix.implementations.MovieDataSourceImplementation
